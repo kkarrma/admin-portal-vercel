9ebe0c26f85b3b0c9eb923ddf6b65ecc0bb64b98
Mark_Gara <garabilezmark0130@gmail.com>
2025-03-12 14:37:04 +0800

    refund init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./webpages/Login"
+import Refund from "./webpages/Refund"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Route path="/sign-in" element={<Login mode="SIGN_IN"/>} />
         <Route path="/sign-up/merchant" element={<Login mode="SIGN_UP_MERCHANT"/>} />
         <Route path="/sign-in/merchant" element={<Login mode="SIGN_IN_MERCHANT"/>} />
+        <Route path="/refund" element={<Refund />} />
       </Routes>
     </BrowserRouter>
   )
