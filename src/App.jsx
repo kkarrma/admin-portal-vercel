92dbe6402873b94594805cb2a0c079d92bee0f2b
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2025-03-17 15:30:26 +0800

    updates in sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ function App() {
       <Sidebar>
         <Routes>
           <Route path="/sign-in" element={<Login mode="SIGN_IN" />} />
-          <Route path="/sign-up/merchant" element={<Login mode="SIGN_UP_MERCHANT" />} />
-          <Route path="/sign-in/merchant" element={<Login mode="SIGN_IN_MERCHANT" />} />
+          <Route path="/sign-up/merchant" element={<Login mode="SIGN_UP" />} />
           <Route path="/" element={<Dashboard />} />
         </Routes>
       </Sidebar>
