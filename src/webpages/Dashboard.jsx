82677d413631a827fca6a33d613ff0681d0987df
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2025-03-25 15:05:32 +0800

    date and time

--- src/webpages/Dashboard.jsx
+++ src/webpages/Dashboard.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import default_user_icon from "../assets/default_user_icon.png"
 
 export default function Dashboard() {
     const [username, setUsername] = useState("Super Admin");
+    const [role, setRole] = useState("Admin");
+    const [dateTime, setDateTime] = useState(getCurrentDateTime());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const seconds = new Date().getSeconds();
+
+            if (seconds === 0) {
+                setDateTime(getCurrentDateTime());
+            }
+
+            if (seconds === 1) {
+                setDateTime(getCurrentDateTime());
+            }
+
+            const currentTime = getCurrentDateTime();
+            if (dateTime !== currentTime) {
+                setDateTime(currentTime);
+            }
+        }, 1000);
+
+        return () => clearInterval(intervalId); // Cleanup on unmount
+    }, [dateTime]);
+
     return (
         <div className="px-[72px] pt-10">
             <div className="flex flex-row">
                 <div className="flex flex-col flex-1">
-                    <p className="font-poppins text-xs font-light">Welcome back!</p>
-                    <p className="font-poppins text-base font-medium">Welcome, {username}! 👋🏼</p>
+                    <p className="font-montserrat text-3xl">{role} Portal</p>
                 </div>
                 <div className="flex flex-row items-center gap-4">
                     <img src={default_user_icon} className="w-10 h-10 rounded-full" />
@@ -17,4 +40,20 @@ export default function Dashboard() {
             </div>
         </div>
     );
+}
+
+function getCurrentDateTime() {
+    const now = new Date();
+
+    const month = now.toLocaleString('en-US', { month: 'long' });
+    const year = now.getFullYear();
+
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'pm' : 'am';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    return `${month} ${year} | ${hours}:${minutes} ${ampm}`;
 }
