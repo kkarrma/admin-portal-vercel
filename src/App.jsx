bfb6b62b992d1c69d1fa0ef009e1534e99d16bc4
Asanagi <alcaydekjared@gmail.com>
2025-04-10 10:14:52 +0800

    Added product management page, filter beside search and modal need changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,92 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Login from "./webpages/Login"
-import Sidebar from "./components/Sidebar"
-import Dashboard from "./webpages/Dashboard"
-import { useState } from "react"
-import Refund from "./webpages/Refund"
-import RefundV2 from "./webpages/Refund-v2"
-import ReturnRefund from "./webpages/Return-refund"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./webpages/Login";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./webpages/Dashboard";
+import { useState } from "react";
+import Refund from "./webpages/Refund";
+import RefundV2 from "./webpages/Refund-v2";
+import ReturnRefund from "./webpages/Return-refund";
+import ProductManagement from "./webpages/Product-management";
 
 function App() {
+    const [accountStatus, setAccountStatus] = useState(false);
 
-  const [accountStatus, setAccountStatus] = useState(false);
+    return (
+        <BrowserRouter>
+            <Sidebar
+                accountStatus={{
+                    value: accountStatus,
+                    setter: setAccountStatus,
+                }}
+            >
+                <Routes>
+                    <Route
+                        path="/sign-in"
+                        element={
+                            <Login
+                                mode="SIGN_IN"
+                                accountStatus={{
+                                    value: accountStatus,
+                                    setter: setAccountStatus,
+                                }}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/sign-in/merchant"
+                        element={
+                            <Login
+                                mode="SIGN_IN_MERCHANT"
+                                accountStatus={{
+                                    value: accountStatus,
+                                    setter: setAccountStatus,
+                                }}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/sign-up/merchant"
+                        element={
+                            <Login
+                                mode="SIGN_UP"
+                                accountStatus={{
+                                    value: accountStatus,
+                                    setter: setAccountStatus,
+                                }}
+                            />
+                        }
+                    />
 
-  return (
-    <BrowserRouter>
-      <Sidebar accountStatus={{ value: accountStatus, setter: setAccountStatus }}>
-        <Routes>
-          <Route path="/sign-in" element={<Login mode="SIGN_IN" accountStatus={{ value: accountStatus, setter: setAccountStatus }} />} />
-          <Route path="/sign-in/merchant" element={<Login mode="SIGN_IN_MERCHANT" accountStatus={{ value: accountStatus, setter: setAccountStatus }} />} />
-          <Route path="/sign-up/merchant" element={<Login mode="SIGN_UP" accountStatus={{ value: accountStatus, setter: setAccountStatus }} />} />
-          
-          <Route path="/orders/return-and-refund" element={<Refund />} />
-          <Route path="/orders/return-v2" element={<RefundV2 />} />
-          <Route path="/orders/return-refund" element={<ReturnRefund />} />
-          
-          {/* Protected Routes */}
-          <Route path="/" element={
-            accountStatus ?
-              (<Dashboard />)
-              :
-              (<Navigate to="/sign-in" replace />)} />
-        </Routes>
-      </Sidebar>
-    </BrowserRouter>
-  )
+                    <Route
+                        path="/orders/return-and-refund"
+                        element={<Refund />}
+                    />
+                    <Route path="/orders/return-v2" element={<RefundV2 />} />
+                    <Route
+                        path="/orders/return-refund"
+                        element={<ReturnRefund />}
+                    />
+
+                    <Route
+                        path="/management/product"
+                        element={<ProductManagement />}
+                    />
+
+                    {/* Protected Routes */}
+                    <Route
+                        path="/"
+                        element={
+                            accountStatus ? (
+                                <Dashboard />
+                            ) : (
+                                <Navigate to="/sign-in" replace />
+                            )
+                        }
+                    />
+                </Routes>
+            </Sidebar>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
